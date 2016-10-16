0cd2f12a22ab4eeb1d53cdd4ca6971d42ccdc274
madcocomo <madcocomo@gmail.com>
2016-10-16 20:56:52 +0800

    long package test case

--- src/test/groovy/KeyGeneratorSpec.groovy
+++ src/test/groovy/KeyGeneratorSpec.groovy
@@ -10,9 +10,10 @@ class KeyGeneratorSpec extends Specification {
         then:
         key == expect
         where:
-        className                               |method         ||expect
-        "5678901234567890123456789"             |"1234567890"   ||"DBO\$5678901234567890123456789.1234567890"
-        "my.package.Repository"                 |"saveItem"     ||"DBO\$my.package.Repository.saveItem"
-        "my.deep.deep.deep.package.Repository"  |"saveItem"     ||"DBO\$Repository.saveItem"
+        className                                   |method         ||expect
+        "5678901234567890123456789"                 |"1234567890"   ||"DBO\$5678901234567890123456789.1234567890"
+        "my.package.Repository"                     |"saveItem"     ||"DBO\$my.package.Repository.saveItem"
+        "my.deep.deep.deep.package.Repository"      |"saveItem"     ||"DBO\$Repository.saveItem"
+        "my.package.VeryVeryVeryLoooooogClassName"  |"saveItem"     ||"DBO\$saveItem"
     }
 }
