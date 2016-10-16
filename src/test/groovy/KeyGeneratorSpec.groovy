c41894d8373223485bda7cc765f78066c33156de
U-FLATIRONS\vwu <vwu@SHA-BK5CWZ1.flatironssolutions.com>
2016-10-16 17:20:36 +0800

    init version

---
+++ src/test/groovy/KeyGeneratorSpec.groovy
@@ -0,0 +1,10 @@
+import spock.lang.Specification
+
+class KeyGeneratorSpec extends Specification {
+    def "my first test"() {
+        when:
+        int actual = 2
+        then:
+        1+2 == actual
+    }
+}
