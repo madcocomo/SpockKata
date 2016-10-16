1a460bc8ce3fb5e5dbfd99e19ccb231ae2a9f249
madcocomo <madcocomo@gmail.com>
2016-10-16 18:32:14 +0800

    Refactor test

--- src/test/groovy/KeyGeneratorSpec.groovy
+++ src/test/groovy/KeyGeneratorSpec.groovy
@@ -2,21 +2,16 @@ import spock.lang.Specification
 import vic.kata.KeyGenerator
 
 class KeyGeneratorSpec extends Specification {
-    def "Key should contents header, package, and method"() {
-        given:
-        KeyGenerator generator = new KeyGenerator("DBO\$", 40);
-        when:
-        String key = generator.genKey "my.package.Repository", "saveItem"
-        then:
-        key == "DBO\$my.package.Repository.saveItem"
-    }
-
     def "Key should not longer than limitation"() {
         given:
         KeyGenerator generator = new KeyGenerator("DBO\$", 40);
         when:
-        String key = generator.genKey "my.deep.deep.deep.package.Repository", "saveItem"
+        String key = generator.genKey className, method
         then:
-        key == "DBO\$Repository.saveItem"
+        key == expect
+        where:
+        className                               |method     ||expect
+        "my.package.Repository"                 |"saveItem" ||"DBO\$my.package.Repository.saveItem"
+        "my.deep.deep.deep.package.Repository"  |"saveItem" ||"DBO\$Repository.saveItem"
     }
 }
