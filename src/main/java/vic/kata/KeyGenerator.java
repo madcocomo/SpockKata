b32eb9b40ff7feec1869a9359ea33c63b4e067bf
madcocomo <madcocomo@gmail.com>
2016-10-16 18:25:49 +0800

    Remove package if too long

--- src/main/java/vic/kata/KeyGenerator.java
+++ src/main/java/vic/kata/KeyGenerator.java
@@ -4,12 +4,22 @@ import java.text.MessageFormat;
 
 public class KeyGenerator {
     private String header;
+    private int length;
 
     public KeyGenerator(String header, int length) {
         this.header = header;
+        this.length = length;
     }
 
     public String genKey(String className, String method) {
-        return MessageFormat.format("{0}{1}.{2}", header, className, method);
+        String key = MessageFormat.format("{0}{1}.{2}", header, className, method);
+        if (key.length() > length) {
+            return MessageFormat.format("{0}{1}.{2}", header, classNameWithoutPackage(className), method);
+        }
+        return key;
+    }
+
+    private String classNameWithoutPackage(String className) {
+        return className.substring(className.lastIndexOf(".")+1);
     }
 }
