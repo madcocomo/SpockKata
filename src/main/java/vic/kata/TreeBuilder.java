0118fc7a61bdfdc20435e96c83f5ff0f87c7f19b
madcocomo <madcocomo@gmail.com>
2016-11-03 22:51:55 +0800

    Tree builder test 2 refactor

--- src/main/java/vic/kata/TreeBuilder.java
+++ src/main/java/vic/kata/TreeBuilder.java
@@ -3,8 +3,12 @@ package vic.kata;
 public class TreeBuilder {
 
     public TreeNode build(String text) {
-        if (text.contains(","))
-            return new TreeNode(text.split(",")[0], new TreeNode(text.split(",")[1]));
-        return new TreeNode(text);
+        String[] parts = text.split(",");
+        String root = parts[0];
+        if (text.contains(",")) {
+            String left = parts[1];
+            return new TreeNode(root, new TreeNode(left));
+        }
+        return new TreeNode(root);
     }
 }
