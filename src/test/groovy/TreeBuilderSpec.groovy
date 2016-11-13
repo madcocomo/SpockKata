9b1730aff58bc0eb8e12276071845c49c3f22021
madcocomo <madcocomo@gmail.com>
2016-11-13 22:58:55 +0800

    more test

--- src/test/groovy/TreeBuilderSpec.groovy
+++ src/test/groovy/TreeBuilderSpec.groovy
@@ -56,6 +56,7 @@ class TreeBuilderSpec extends Specification {
         "A"          || ["A", "", ""]
         "A,B,(C,C1)" || ["A", "B", "(C,C1)"]
         "A,B,(C,C1,(C2,C22))" || ["A", "B", "(C,C1,(C2,C22))"]
+        "A,B,(C,(C1,C11),C2)" || ["A", "B", "(C,(C1,C11),C2)"]
     }
 
 }
