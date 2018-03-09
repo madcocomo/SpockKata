e34a2fda5a937123a14dcac957541cc1baa2adcf
Vic Wu <madcocomo@gmail.com>
2018-03-09 09:19:50 +0800

    bulls and cows, refactor test

--- src/test/groovy/BullsAndCowsSpec.groovy
+++ src/test/groovy/BullsAndCowsSpec.groovy
@@ -2,13 +2,13 @@ import spock.lang.Specification
 import vic.kata.BullsAndCows
 
 class BullsAndCowsSpec extends Specification {
-    def "will get 0 bull comparing 3 & 4"() {
+    def "will get hint comparing secret & guess"() {
         expect:
-        BullsAndCows.getHint("3", "4") == "0A0B"
+        BullsAndCows.getHint(secret, guess) == expect
+        where:
+        expect || secret | guess | DESC
+        "0A0B" || "3"    | "4"   | "1 digit, different"
+        "1A0B" || "3"    | "3"   | "1 digit, same"
     }
 
-    def "will get 1 bull comparing 3 & 4"() {
-        expect:
-        BullsAndCows.getHint("3", "3") == "1A0B"
-    }
 }
