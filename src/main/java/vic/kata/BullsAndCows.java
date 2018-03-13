241c3af28dd19cefc7b830e8a3002701cb9b1417
Vic Wu <madcocomo@gmail.com>
2018-03-13 15:24:25 +0800

    bulls and cows, improvement refactor 3

--- src/main/java/vic/kata/BullsAndCows.java
+++ src/main/java/vic/kata/BullsAndCows.java
@@ -10,7 +10,7 @@ public class BullsAndCows {
         int bulls = 0;
         int cows = 0;
         Map<Character, Integer> secretCharCounts = new HashMap<>();
-        List<Character> guessChars = new ArrayList<>();
+        Map<Character, Integer> guessCharCounts = new HashMap<>();
         for (int i = 0; i < guess.length(); i++ ) {
             Character c = secret.charAt(i);
             if (c == guess.charAt(i)) {
@@ -18,17 +18,19 @@ public class BullsAndCows {
             } else {
                 int count = secretCharCounts.getOrDefault(c,0);
                 secretCharCounts.put(c, count+1);
-                guessChars.add(guess.charAt(i));
+
+                Character c2 = guess.charAt(i);
+                int count2 = guessCharCounts.getOrDefault(c2,0);
+                guessCharCounts.put(c2, count2+1);
             }
         }
 
         for (Map.Entry<Character, Integer> entry: secretCharCounts.entrySet()) {
             Character c = entry.getKey();
-            for (int i = 0; i < entry.getValue(); i++) {
-                if (guessChars.contains(c)) {
-                    cows += 1;
-                    guessChars.remove(c);
-                }
+            if (guessCharCounts.getOrDefault(c, 0) < entry.getValue()) {
+                cows += guessCharCounts.getOrDefault(c,0);
+            } else {
+                cows += entry.getValue();
             }
         }
         return bulls +"A" + cows + "B";
