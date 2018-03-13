b367067f8f8e6ccc2edbb36110d6c1b560be4638
Vic Wu <madcocomo@gmail.com>
2018-03-13 15:18:08 +0800

    bulls and cows, improvement refactor 2

--- src/main/java/vic/kata/BullsAndCows.java
+++ src/main/java/vic/kata/BullsAndCows.java
@@ -1,28 +1,34 @@
 package vic.kata;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BullsAndCows {
     public static String getHint(String secret, String guess) {
         int bulls = 0;
         int cows = 0;
-        List<Character> secretChars = new ArrayList<>();
+        Map<Character, Integer> secretCharCounts = new HashMap<>();
         List<Character> guessChars = new ArrayList<>();
         for (int i = 0; i < guess.length(); i++ ) {
             Character c = secret.charAt(i);
             if (c == guess.charAt(i)) {
                 bulls += 1;
             } else {
-                secretChars.add(c);
+                int count = secretCharCounts.getOrDefault(c,0);
+                secretCharCounts.put(c, count+1);
                 guessChars.add(guess.charAt(i));
             }
         }
 
-        for (Character c : secretChars) {
-            if (guessChars.contains(c)) {
-                cows += 1;
-                guessChars.remove(c);
+        for (Map.Entry<Character, Integer> entry: secretCharCounts.entrySet()) {
+            Character c = entry.getKey();
+            for (int i = 0; i < entry.getValue(); i++) {
+                if (guessChars.contains(c)) {
+                    cows += 1;
+                    guessChars.remove(c);
+                }
             }
         }
         return bulls +"A" + cows + "B";
