1927f0aa9795059801e4547f49633c2a76fa9fb9
Vic Wu <madcocomo@gmail.com>
2018-03-13 15:28:35 +0800

    bulls and cows, improvement refactor 4

--- src/main/java/vic/kata/BullsAndCows.java
+++ src/main/java/vic/kata/BullsAndCows.java
@@ -1,8 +1,6 @@
 package vic.kata;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BullsAndCows {
@@ -12,26 +10,20 @@ public class BullsAndCows {
         Map<Character, Integer> secretCharCounts = new HashMap<>();
         Map<Character, Integer> guessCharCounts = new HashMap<>();
         for (int i = 0; i < guess.length(); i++ ) {
-            Character c = secret.charAt(i);
-            if (c == guess.charAt(i)) {
+            Character secretC = secret.charAt(i);
+            Character guessC = guess.charAt(i);
+            if (secretC == guess.charAt(i)) {
                 bulls += 1;
             } else {
-                int count = secretCharCounts.getOrDefault(c,0);
-                secretCharCounts.put(c, count+1);
-
-                Character c2 = guess.charAt(i);
-                int count2 = guessCharCounts.getOrDefault(c2,0);
-                guessCharCounts.put(c2, count2+1);
+                secretCharCounts.put(secretC, secretCharCounts.getOrDefault(secretC,0) +1);
+                guessCharCounts.put(guessC, guessCharCounts.getOrDefault(guessC,0) +1);
             }
         }
 
-        for (Map.Entry<Character, Integer> entry: secretCharCounts.entrySet()) {
-            Character c = entry.getKey();
-            if (guessCharCounts.getOrDefault(c, 0) < entry.getValue()) {
-                cows += guessCharCounts.getOrDefault(c,0);
-            } else {
-                cows += entry.getValue();
-            }
+        for (Character c : secretCharCounts.keySet()) {
+            Integer secretCount = secretCharCounts.get(c);
+            Integer guessCount = guessCharCounts.getOrDefault(c, 0);
+            cows += Math.min(secretCount, guessCount);
         }
         return bulls +"A" + cows + "B";
     }
