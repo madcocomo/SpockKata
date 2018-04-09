0291467a33903a002c4b793361ee1fcdd5753be2
Vic Wu <madcocomo@gmail.com>
2018-04-09 17:21:22 +0800

    leetcode 321 refactor

--- src/main/java/vic/kata/MaximumNumberFrom2Arrays.java
+++ src/main/java/vic/kata/MaximumNumberFrom2Arrays.java
@@ -4,23 +4,10 @@ public class MaximumNumberFrom2Arrays {
     public int[] maxNumberOf(int[] num1, int[] num2, int k) {
         int[] result = new int[k];
         for (int i = 0, maxP1 = 0, maxP2 = 0; i < k; i++) {
-            int max1 = -1, max2 = -1;
-            for (int p = maxP1;
-                 p < Math.min(num1.length, num1.length + num2.length - (k - i) + 1);
-                 p++) {
-                if (max1 < num1[p]) {
-                    maxP1 = p;
-                    max1 = num1[p];
-                }
-            }
-            for (int p = maxP2;
-                 p < Math.min(num2.length, num2.length + num1.length - (k - i) + 1);
-                 p++) {
-                if (max2 < num2[p]) {
-                    maxP2 = p;
-                    max2 = num2[p];
-                }
-            }
+            maxP1 = getMaxIndex(num1, maxP1, getLimit(k, i, num1.length, num2.length));
+            maxP2 = getMaxIndex(num2, maxP2, getLimit(k, i, num2.length, num1.length));
+            int max1 = valueOrMinimize(num1, maxP1);
+            int max2 = valueOrMinimize(num2, maxP2);
             if (max1 > max2) {
                 result[i] = max1;
                 maxP1++;
@@ -31,5 +18,23 @@ public class MaximumNumberFrom2Arrays {
         }
 
         return result;
+    }
+
+    private int getMaxIndex(int[] num, int start, int limit) {
+        int maxIndex = start;
+        for (int p = start; p < limit; p++) {
+            if (valueOrMinimize(num, maxIndex) < num[p]) {
+                maxIndex = p;
+            }
+        }
+        return maxIndex;
+    }
+
+    private int getLimit(int k, int resultIndex, int length, int anotherLength) {
+        return Math.min(length, length + anotherLength - (k - resultIndex) + 1);
+    }
+
+    private int valueOrMinimize(int[] num, int index) {
+        return index < num.length ? num[index] : -1;
     }
 }
