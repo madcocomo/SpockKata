f3387bda9f0feb2fa132a70265d5e7b2981614fb
Vic Wu <madcocomo@gmail.com>
2018-04-09 10:19:27 +0800

    leetcode 321 1 array at 2nd refactor.

--- src/main/java/vic/kata/MaximumNumberFrom2Arrays.java
+++ src/main/java/vic/kata/MaximumNumberFrom2Arrays.java
@@ -3,22 +3,24 @@ package vic.kata;
 public class MaximumNumberFrom2Arrays {
     public int[] maxNumberOf(int[] num1, int[] num2, int k) {
         int[] result = new int[k];
-        for (int i = 0, maxP = 0; i < k; i++, maxP++) {
-            for (int p = maxP; p < num1.length - (k - i) + 1; p++) {
-                if (num1[maxP] < num1[p]) {
-                    maxP = p;
+        for (int i = 0, maxP1 = 0, maxP2 = 0; i < k; i++) {
+            for (int p = maxP1; p < num1.length - num2.length - (k - i) + 1; p++) {
+                if (num1[maxP1] < num1[p]) {
+                    maxP1 = p;
                 }
             }
-            if (num1.length > maxP) {
-                result[i] = num1[maxP];
-            }
-            for (int p = maxP; p < num2.length - (k - i) + 1; p++) {
-                if (num2[maxP] < num2[p]) {
-                    maxP = p;
+            for (int p = maxP2; p < num2.length - num1.length - (k - i) + 1; p++) {
+                if (num2[maxP2] < num2[p]) {
+                    maxP2 = p;
                 }
             }
-            if (num2.length > maxP) {
-                result[i] = num2[maxP];
+            if (num1.length > maxP1) {
+                result[i] = num1[maxP1];
+                maxP1++;
+            }
+            if (num2.length > maxP2) {
+                result[i] = num2[maxP2];
+                maxP2++;
             }
         }
 
