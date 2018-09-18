fbc34b5e5158843bd9a015937a847c5567931b83
wuke <wuke03845@hellobike.com>
2018-09-18 10:48:33 +0800

    First test

---
+++ src/main/java/vic/kata/FirstMissingPositive.java
@@ -0,0 +1,7 @@
+package vic.kata;
+
+public class FirstMissingPositive {
+    public static int of(int[] numbers) {
+        return 0;
+    }
+}
---
+++ src/test/groovy/FirstMissingPositiveSpec.groovy
@@ -0,0 +1,14 @@
+import spock.lang.Specification
+import vic.kata.FirstMissingPositive
+
+//Leetcode 41
+class FirstMissingPositiveSpec extends Specification {
+    def "will be '#expect' from #numbers"() {
+        expect:
+        FirstMissingPositive.of(numbers as int[]) == expect
+        where:
+        expect || numbers
+        1      || [1]
+    }
+
+}
