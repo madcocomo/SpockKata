a5305a65157cf65bc894c6639a647153dcba6aa7
wuke <wuke03845@hellobike.com>
2018-09-18 14:37:17 +0800

    11th test, failed case from leetcode

--- src/test/groovy/FirstMissingPositiveSpec.groovy
+++ src/test/groovy/FirstMissingPositiveSpec.groovy
@@ -20,6 +20,6 @@ class FirstMissingPositiveSpec extends Specification {
         5      || [3,4,2,1]
         3      || [4,5,2,1]
         4      || [3,2,2,1]
+        1      || [3,4,2,4,4,3,3]
     }
-
 }
